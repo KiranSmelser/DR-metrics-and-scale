added datasets and fixed normalization to accomodate new datasets

main.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import manifold
 import umap.umap_ as umap
 from sklearn import random_projection
+from sklearn.preprocessing import MinMaxScaler
 from datasets import *
 from stress import *
 from embeddings import *
@@ -15,29 +16,29 @@ def main():
     scalars = np.linspace(0.0, range, 200)
 
     for dataset_name, (X, Y) in datasets_dict.items():
-        # Normalize
-        X /= np.max(X, axis=0)
+        # Min-Max normalization
+        X = MinMaxScaler().fit_transform(X)
 
         # t-SNE
         tsne = manifold.TSNE(n_components=2, perplexity=40,
-                             init='pca', random_state=42)
+                             init='pca', random_state=23)
         fit_tsne = tsne.fit_transform(X)
         tsne_stresses = evaluate_scaling(fit_tsne, X, scalars)
 
         # UMAP
-        reducer = umap.UMAP(random_state=42)
+        reducer = umap.UMAP(random_state=23)
         fit_umap = reducer.fit_transform(X)
         umap_stresses = evaluate_scaling(fit_umap, X, scalars)
 
         # MDS
         mds = manifold.MDS(n_components=2, n_init=1,
-                           max_iter=120, n_jobs=2, random_state=42)
+                           max_iter=120, n_jobs=2, random_state=23)
         fit_mds = mds.fit_transform(X)
         mds_stresses = evaluate_scaling(fit_mds, X, scalars)
 
         # Random Projection
         transformer = random_projection.GaussianRandomProjection(
-            n_components=2, random_state=42)
+            n_components=2)
         fit_random = transformer.fit_transform(X)
         random_stresses = evaluate_scaling(fit_random, X, scalars)
 
@@ -55,6 +56,13 @@ def main():
         # Plot Sheppard Diagrams
         shepard_scalars, shepard_corrs = shepard(
             X, results, range, dataset_name)
+        shepard_scalars_stresses = [results['tsne'][1][np.argmin(np.abs(scalars - shepard_scalars[0]))],
+                                    results['umap'][1][np.argmin(
+                                        np.abs(scalars - shepard_scalars[1]))],
+                                    results['mds'][1][np.argmin(
+                                        np.abs(scalars - shepard_scalars[2]))],
+                                    results['random'][1][np.argmin(
+                                        np.abs(scalars - shepard_scalars[3]))]]
 
         # Orderings
         rankings = set(orderings(scalars, results))
@@ -65,7 +73,8 @@ def main():
         for algo, (fit, stresses) in results.items():
             np.savetxt(f'{dataset_name}/{algo}_fit.txt', fit)
             np.savetxt(f'{dataset_name}/{algo}_stresses.txt', stresses)
-        np.savetxt(f'{dataset_name}/shepard_scalars.txt', shepard_scalars)
+        np.savetxt(f'{dataset_name}/shepard_scalars.txt',
+                   shepard_scalars_stresses)
         np.savetxt(f'{dataset_name}/shepard_corrs.txt', shepard_corrs)
         np.savetxt(f'{dataset_name}/rankings.txt', list(rankings), fmt='%s')
         np.savetxt(f'{dataset_name}/min_stresses.txt',

viz.py
@@ -23,7 +23,7 @@ scores of the dimensionality reduction techniques within an "interesting" range.
     return rankings
 
 
-def summary_plot(scalars, sheppard, techniques, dataset):
+def summary_plot(scalars, shepard, techniques, dataset):
     """Function that creates a summary plot used to compared the different 
 dimensionality reduction techniques."""
     tsne_data = pd.DataFrame({
@@ -59,15 +59,15 @@ dimensionality reduction techniques."""
     plt.xlabel('Scalar')
     plt.ylabel('Normalized Stress')
 
-    # Points where the Sheppard Goodness correlation coefficients fall
-    tsne_point, = plt.plot(sheppard[0], techniques['tsne'][1][np.argmin(
-        np.abs(scalars - sheppard[0]))], 'x', color='blue', markersize=15)
-    umap_point, = plt.plot(sheppard[1], techniques['umap'][1][np.argmin(
-        np.abs(scalars - sheppard[1]))], 'x', color='orange', markersize=15)
-    mds_point, = plt.plot(sheppard[2], techniques['mds'][1][np.argmin(
-        np.abs(scalars - sheppard[2]))], 'x', color='green', markersize=15)
-    random_point, = plt.plot(sheppard[2], techniques['random'][1][np.argmin(
-        np.abs(scalars - sheppard[2]))], 'x', color='red', markersize=15)
+    # Points where the shepard Goodness correlation coefficients fall
+    tsne_point, = plt.plot(shepard[0], techniques['tsne'][1][np.argmin(
+        np.abs(scalars - shepard[0]))], 'x', color='blue', markersize=15)
+    umap_point, = plt.plot(shepard[1], techniques['umap'][1][np.argmin(
+        np.abs(scalars - shepard[1]))], 'x', color='orange', markersize=15)
+    mds_point, = plt.plot(shepard[2], techniques['mds'][1][np.argmin(
+        np.abs(scalars - shepard[2]))], 'x', color='green', markersize=15)
+    random_point, = plt.plot(shepard[2], techniques['random'][1][np.argmin(
+        np.abs(scalars - shepard[2]))], 'x', color='red', markersize=15)
 
     # Initial order
     initial_scalar_index = np.argmin(np.abs(scalars - 1))
@@ -80,8 +80,8 @@ dimensionality reduction techniques."""
     ranking_str = "\n" + \
         "\n".join(
             [f"{i+1}. {initial_ranking[i]}" for i in range(len(initial_ranking))])
-    plt.annotate(f'Initial Ranking: {ranking_str}', xy=(1, 0.5), xycoords='axes fraction',
-                 xytext=(1/max(scalars)-.015, .7), textcoords='axes fraction',
+    plt.annotate(f'Initial Ranking: {ranking_str}', xy=(1, 0.2), xycoords='axes fraction',
+                 xytext=(1/max(scalars)-.015, .2), textcoords='axes fraction',
                  bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
 
     # Check for changes in the order
@@ -98,17 +98,11 @@ dimensionality reduction techniques."""
                     [f"{j+1}. {current_ranking[j]}" for j in range(len(current_ranking))])
             plt.axvline(x=scalars[i], color='grey', linestyle='--')
             plt.annotate(f'Ranking when scaled by {scalars[i]:.2f}: {ranking_str}',
-                         xy=(scalars[i], np.mean([techniques['tsne'][0][i],
-                                                  techniques['umap'][0][i],
-                                                  techniques['mds'][0][i],
-                                                  techniques['random'][0][i]])),
-                         xytext=(scalars[i],
-                                 np.mean([techniques['tsne'][1][i],
-                                          techniques['umap'][1][i],
-                                          techniques['mds'][1][i],
-                                          techniques['random'][1][i]])
-                                 * annotations),
-                         textcoords=('data'),
+                         xy=(1, .1 * annotations),
+                         xycoords='axes fraction',
+                         xytext=(scalars[i]/max(scalars),
+                                 .12 * annotations),
+                         textcoords=('axes fraction'),
                          bbox=dict(facecolor='white',
                                    edgecolor='black',
                                    boxstyle='round,pad=0.5'))
@@ -117,8 +111,8 @@ dimensionality reduction techniques."""
 
     handles, labels = plt.gca().get_legend_handles_labels()
     handles.extend([tsne_point, umap_point, mds_point, random_point])
-    labels.extend(['t-SNE Sheppard Scalar',
-                  'UMAP Sheppard Scalar', 'MDS Sheppard Scalar', 'Random Sheppard Scalar'])
+    labels.extend(['t-SNE shepard Scalar',
+                  'UMAP shepard Scalar', 'MDS shepard Scalar', 'Random shepard Scalar'])
     plt.legend(handles=handles, labels=labels)
     plt.savefig(f'{dataset}/summary_plot.png')
     plt.clf()