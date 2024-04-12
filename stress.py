dfc9bd9eee75cc087cd84987309d3bfa96f66ad2
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2024-04-12 12:35:24 -0700

    Added functions to compute the exact min scale factor via derivative

--- stress.py
+++ stress.py
@@ -40,3 +40,26 @@ def find_optimal_scalars(scalars, techniques):
             scalars[techniques['umap'][1].index(min(techniques['umap'][1]))],
             scalars[techniques['mds'][1].index(min(techniques['mds'][1]))],
             scalars[techniques['random'][1].index(min(techniques['random'][1]))]]
+
+def find_min_stress_exact(X,Y):
+    """
+    Given high dimensional data Y, low dimensional data X, 
+    calculates the minimum value of the stress curve exactly.
+    """
+    D_low  = pdist(X)
+    D_high = pdist(Y)
+
+    alpha = find_optimal_scalars_exact(D_low, D_high)
+
+    return normalized_stress(D_high, alpha * D_low)    
+
+def find_optimal_scalars_exact(D_low, D_high):
+    """
+    Given two distance matrices D_low, D_high, computes 
+    the optimal scalar value to multiply D_low by so that 
+    the normalized stress between them is minimum.
+    """
+
+    D_low_triu  = D_low[np.triu_indices( D_low.shape[0], k=1 )]
+    D_high_triu = D_high[np.triu_indices(D_high.shape[0], k=1)]
+    return np.sum(D_low_triu / D_high_triu) / np.sum(np.square(D_low_triu / D_high_triu))
