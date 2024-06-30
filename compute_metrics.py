751799b06b9a13383ce9c465fcff0284dd389f3f
Mickey253 <jacob_l._miller@hotmail.com>
2024-06-30 13:29:51 -0500

    adding a few minor changes

--- compute_metrics.py
+++ compute_metrics.py
@@ -16,13 +16,13 @@ def compute_all_metrics():
             if "fashion_mnist" in datasetStr:
                 datasetName = "fashion_mnist"            
             X = np.load(f"datasets/{datasetName}.npy")
-            X *= 10
             
             print(f"Dataset: {datasetName}, size: {X.shape}")
             for i in range(10):
                 datasetResults = dict()
                 for alg in ["MDS", "TSNE", 'UMAP', "RANDOM"]:        
                     Y = np.load(f"embeddings/{datasetName}_{alg}_{i}.npy")
+                    Y *= 10
 
                     M = Metrics(X,Y)
 
@@ -38,10 +38,52 @@ def compute_all_metrics():
                 datasetResults = {key: float(val) for key,val in datasetResults.items()}            
                 results[f'{datasetName}_{i}'] = datasetResults    
 
-            with open("out10x.json", 'w') as fdata:
+            with open("out2x.json", 'w') as fdata:
                 json.dump(results,fdata,indent=4)            
                 
 
 
+
+def test_curve():
+    import pylab as plt
+    results = dict()
+
+    with tqdm.tqdm(total=len(os.listdir('datasets')) * 4) as pbar:
+
+        for datasetStr in os.listdir("datasets"): 
+            datasetName = datasetStr.replace(".npy", "")
+            if "fashion_mnist" in datasetStr:
+                datasetName = "fashion_mnist"            
+            X = np.load(f"datasets/{datasetName}.npy")
+            
+            print(f"Dataset: {datasetName}, size: {X.shape}")
+            datasetResults = dict()
+            fig,ax = plt.subplots()
+            for alg in ["MDS", "TSNE", 'UMAP', "RANDOM"]:        
+                Y = np.load(f"embeddings/{datasetName}_{alg}_0.npy")
+
+                M = Metrics(X,Y)
+
+                rrange = np.linspace(0,10,100)
+                stresses = [M.compute_normalized_stress(alpha=a) for a in rrange]
+
+                ax.plot(rrange,stresses,label=alg)
+
+                # datasetResults[f'{alg}_norm'] = M.compute_normalized_stress()
+
+                pbar.update(1)
+
+            ax.legend()
+            fig.savefig(f"test-figures/{datasetName}.png")
+            plt.close(fig)
+
+
+            # with open("out10x.json", 'w') as fdata:
+            #     json.dump(results,fdata,indent=4)            
+                
+
+
+
 if __name__ == "__main__":
     compute_all_metrics()
+    # test_curve()
